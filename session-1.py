a738472b0cead98eba993bcf8d5bf420c5194be2
carlosleoncode <carlosestebanleonpinilla@gmail.com>
2022-10-01 19:09:18 -0500

    feat: lesson 1

---
+++ session-1.py
@@ -0,0 +1,21 @@
+# Variables
+name = "Carlos"
+print(1)
+print(f'hola {name}')
+print("mundo")
+
+# functions
+def mi_funcion():
+  print('Hola, estoy dentro 1')
+# mi_funcion()
+
+def sumar(numero_1, numero_2):
+  resultado = numero_1 + numero_2
+  print(f"La suma es igual a: {resultado}")
+  # print("La suma es igual a:",resultado)
+# sumar(5, 3)  # -> 8
+
+# Reto.
+# Crear una variable que guarde su nombre y su edad
+# Crear una funcion que sume su edad más 10 y mostrar un mensaje que diga:
+# Hola Jhon, tu edad es 17 y en 10 años vas a tener 27.
